70a977e94b2db4964dc345e74e2ec79fb80474eb
naveenautomationlabs <naveenautomationlabs@naveenautomationlabss-MacBook-Pro.local>
2021-04-22 07:36:13 +0530

    adde to cart page is added

---
+++ src/main/java/com/qa/opencart/pages/AddToCartPage.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class AddToCartPage {
+	
+	WebDriver driver;
+	public AddToCartPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	
+
+}
