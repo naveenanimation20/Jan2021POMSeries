3e42d598029ac557a35f0a421662deeb93a52e01
naveenautomationlabs <naveenautomationlabs@naveenautomationlabss-MacBook-Pro.local>
2021-06-01 21:53:35 +0530

    test

--- src/main/java/com/qa/opencart/pages/TestDemo.java
+++ src/main/java/com/qa/opencart/pages/TestDemo.java
@@ -1,11 +1,16 @@
-public class TestDemo{
-  
- int age = 10;
-  
-  public void getage(){
-   age = 20; 
-    
-  }
-  
-  
+
+package com.qa.opencart.pages;
+
+public class TestDemo {
+	int age = 10;
+
+	public void getage() {
+		age = 20;
+		System.out.println(age);
+
+	}
+
+	public void getName() {
+		System.out.println("naveen");
+	}
 }
