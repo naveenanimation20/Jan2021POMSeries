6b85d21ababdb9cfd4671b8ec50ed4793f03050e
naveenautomationlabs <naveenautomationlabs@naveenautomationlabss-MacBook-Pro.local>
2021-06-02 07:19:31 +0530

    updated Deals Page

--- src/main/java/com/qa/opencart/pages/DealsPage.java
+++ src/main/java/com/qa/opencart/pages/DealsPage.java
@@ -1,14 +1,15 @@
 package com.qa.opencart.pages;
 
-public class DealsPage{
-  
- String page = "Deals";
-  
-  public void getPage(){
-   System.out.println(page); 
-    
-  }
-  
-  
-  
+
+public class DealsPage {
+
+	String page = "Deals";
+	int timeOut = 10;
+
+	public void getPage() {
+		System.out.println(page);
+		System.out.println(timeOut);
+		System.out.println("values are: " + page + ":" + timeOut);
+	}
+
 }
